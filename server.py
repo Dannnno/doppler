059be6052879ba3e78aeb061aedbf9654f49c996
Dan Obermiller <dannnno16@gmail.com>
2015-04-10 19:38:50 -0700

    added map stuff

--- server.py
+++ server.py
@@ -9,4 +9,9 @@ def index():
 	return render_template('example.html')
 
 
+@app.route('/map')
+def map():
+	return render_template(
+		'map.html', API_KEY='AIzaSyBM_DeIkDCDTs9gWa0j_qV4FHQKGD0amYU')
+
 app.run(debug=True)
